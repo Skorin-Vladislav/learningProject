91058814c3a73b13ab1a2b4486e56f20834ee116
HouseOfTheRisingSun <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-08-21 13:03:22 +0300

    Navigation bar

--- src/App.js
+++ src/App.js
@@ -3,34 +3,19 @@ import "./App.css";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Blog from "./components/Blog";
-import Text from "./components/Text";
+import Galary from "./components/Galary";
 import About from "./components/About";
 
 function App() {
   return (
     <div className="wraper">
-      
-
       <NavBar />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link to="/text">Text</Link>
-        </li>
-      </ul>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/text" element={<Text />} />
+        <Route path="/galary" element={<Galary />} />
       </Routes>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,99 @@
 import React, { useState } from "react";
 import "./iconsTest.css";
+import "./NavBar.css";
 import { Link } from "react-router-dom";
+import TerrainIcon from "@material-ui/icons/Terrain";
+import MenuIcon from "@material-ui/icons/Menu";
+//import Button from "@material-ui/core/Button";
+import { Button } from "./Button/Button";
 function NavBar() {
   const [click, setClick] = useState(false);
   const [isActive, setIsActive] = useState(false);
-
+  const [button, setButton] = useState(true);
   const handleClick = () => setClick(!click);
   const handleClick2 = () => setIsActive(!isActive);
   const closeDropDown = () => setClick(false);
-  
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
       <nav className="navbar">
         <div className="container">
           <Link to="/" className="logo">
-            Name placeholder <i></i>
+            Name placeholder
+            {/* TODO: find logo*/}
+            <i class="fab fa-typo3" />
           </Link>
-          <div className="drop-down-icon" onClick={handleClick}>
-            <i className={click ? "one" : "two"}>Class switch</i>
 
-            <button className={isActive ? "one" : "two"} onClick={handleClick2}>
-              Click
-            </button>
+          <div className="drop-down-icon" onClick={handleClick}>
+            <i className={click ? "iconDrp fa-times" : " fa-bars"}>
+              {/* Icon of drop-down*/}
+              <MenuIcon className={"iconDrp"}></MenuIcon>
+              <></>
+            </i>
           </div>
-          <ul className={click ? "active-drop" : "drop-down"}>
+
+          <ul className={click ? "drop-down active" : "drop-down"}>
+            <li className="drop-down-item">
+              {/* <Button
+                className={isActive ? "one" : "two"}
+                onClick={handleClick2}
+              >
+                Click
+              </Button> */}
+            </li>
+            <li className="drop-down-item">
+              <Link to="/" className="drop-down-link" onClick={closeDropDown}>
+                Home(NavBar)
+              </Link>
+            </li>
             <li className="drop-down-item">
               <Link
-                to="/"
+                to="/about"
                 className="drop-down-link"
                 onClick={closeDropDown}
-              ></Link>
+              >
+                About(NavBar)
+              </Link>
+            </li>
+            <li className="drop-down-item">
+              <Link
+                to="/blog"
+                className="drop-down-link"
+                onClick={closeDropDown}
+              >
+                Blog(NavBar)
+              </Link>
+            </li>
+            <li className="drop-down-item">
+              <Link
+                to="/galary"
+                className="drop-down-link"
+                onClick={closeDropDown}
+              >
+                Galary(NavBar)
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/sign-up"
+                className="drop-down-link-mobile"
+                // onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle="btn--outline">Sign-up</Button>}
         </div>
       </nav>
     </>
