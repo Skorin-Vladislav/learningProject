9f1da36791b07628c90781d1a121502b26daf1af
HouseOfTheRisingSun <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-09-04 12:55:55 +0300

    about page WIP

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,16 +6,6 @@ import { Link } from "react-router-dom";
 function HeroSection() {
   return (
     <div className="hero-wrap">
-      {/*!video isnt working
-       <video controls autoPlay loop muted >
-        <source
-          src="https://player.vimeo.com/external/337026530.sd.mp4?s=5e1bec41e7e0c227bd70d529f3e9f27d333d561e&profile_id=164&oauth2_token_id=57447761"
-         
-          type="video/mp4"
-        />
-        Your browser doesn't support video tag.
-      </video> */}
-      {/* <video src="/video1.mp4" autoPlay loop muted /> */}
       <h1>Explore</h1>
       <p>Make your dreams come true</p>
       <div className="hero-buttons">
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,52 @@
 import React from "react";
 import "../../App.css";
+import "./About.css";
 
 export default function About() {
-  return <h1 className="about">About us</h1>;
+  return (
+    <div className="about">
+      <h1>About us</h1>
+      <div className="contentContainer">
+        <div className="textContainer">
+          <p className="paragraphNumber">1</p>
+          <div className="textSection">
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </div>
+        </div>
+        <div className="textContainer">
+          <p className="paragraphNumber">2</p>
+          <div className="textSection">
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </div>
+        </div>
+        <div className="textContainer">
+          <p className="paragraphNumber">3</p>
+          <div className="textSection">
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
